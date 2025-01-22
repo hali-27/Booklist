09d2423094b261cd93d2c4bf05d4a531c3a32a14
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-01-22 15:02:26 +0000

    code tested

--- schemas/Book.js
+++ schemas/Book.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+const isRead = true;
+
+const status = isRead === true ? "read" : "unread";
+
+
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
